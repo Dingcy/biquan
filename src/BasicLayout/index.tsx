a9a2b4d625c7568580a49e70ecba7fa4f6bb211b
dingcy <dingcy@hrtps.com>
2020-12-14 22:53:45 +0800

    first submit

---
+++ src/BasicLayout/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import MainRoutes from './MainRoutes'
+
+export function BasicLayout(){
+  
+    
+  return (
+    <div className="home">
+      首页
+    </div>
+  )
+
+}
+
+export default BasicLayout;
